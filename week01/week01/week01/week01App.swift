7793be42154307d0c05ab0e497d3d41ae20340f9
jjwm10625 <jjwm10625@sookmyung.ac.kr>
2026-03-02 00:11:53 +0900

    [week01] Init SwiftUI project

---
+++ week01/week01/week01/week01App.swift
@@ -0,0 +1,17 @@
+//
+//  week01App.swift
+//  week01
+//
+//  Created by 조영서 on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct week01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
