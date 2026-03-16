074337c8f3fcd85d6765552a3f85098583a5d5ae
Cho Youngseo <jjwm10625@sookmyung.ac.kr>
2026-03-16 18:01:05 +0900

    [chore] 네비게이션 화면 추가

--- week01/week01/week01/Views/ProfileView.swift
+++ week01/week01/week01/Views/ProfileView.swift
@@ -13,51 +13,65 @@ struct ProfileView: View {
     
     let username: String
     @State private var showSheet = false
+    @State private var moveToExample: Bool = false
     
     // MARK: - Body
     
     var body: some View {
-        VStack(spacing: 20){
-            InfoView(username: username)
-            FeedView()
-        }
-        .navigationTitle("Profile")
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
+        NavigationStack {
             
-            ToolbarItem(placement: .topBarLeading) {
-                Button {
-                    showSheet = true
-                } label: {
-                    Image(.instagramIcon)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
+            VStack(spacing: 20){
+                InfoView(username: username)
+                FeedView()
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        showSheet = true
+                    } label: {
+                        Image(.instagramIcon)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 20) {
+                        Button {
+                            moveToExample = true
+                        } label: {
+                            Image(systemName: "plus.square")
+                        }
+                        Image(systemName: "line.3.horizontal")
+                    }
                 }
             }
             
-            ToolbarItem(placement: .topBarTrailing) {
-                HStack(spacing: 20) {
-                    Image(systemName: "plus.square")
-                    Image(systemName: "line.3.horizontal")
+            // MARK: - Sheet
+            
+            .sheet(isPresented: $showSheet) {
+                VStack {
+                    
+                    Text("수민언니 바보ㅋㅋ")
+                        .font(.pretendard(.medium, size: 20))
+                        .padding()
+                    
+                    Button("닫기") {
+                        showSheet = false
+                    }
                 }
+                .presentationDetents([.medium])
             }
-        }
-        
-        // MARK: - Sheet
-        
-        .sheet(isPresented: $showSheet) {
-            VStack {
-                
-                Text("수민언니 바보ㅋㅋ")
-                    .font(.pretendard(.medium, size: 20))
-                    .padding()
-                
-                Button("닫기") {
-                    showSheet = false
-                }
+            
+            // MARK: - Navigation
+            
+            .navigationDestination(isPresented: $moveToExample) {
+                ExampleView()
             }
-            .presentationDetents([.medium])
         }
     }
 }
