33625ff4eda6b27e2dc4023d0a78c2fea9f613af
Cho Youngseo <jjwm10625@sookmyung.ac.kr>
2026-03-16 18:06:00 +0900

    [chore] 중복되는 NavigationStack 삭제

--- week01/week01/week01/Views/ProfileView.swift
+++ week01/week01/week01/Views/ProfileView.swift
@@ -18,60 +18,57 @@ struct ProfileView: View {
     // MARK: - Body
     
     var body: some View {
-        NavigationStack {
+        VStack(spacing: 20){
+            InfoView(username: username)
+            FeedView()
+        }
+        .navigationTitle("Profile")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
             
-            VStack(spacing: 20){
-                InfoView(username: username)
-                FeedView()
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    showSheet = true
+                } label: {
+                    Image(.instagramIcon)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                }
             }
-            .navigationTitle("Profile")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                
-                ToolbarItem(placement: .topBarLeading) {
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                HStack(spacing: 20) {
                     Button {
-                        showSheet = true
+                        moveToExample = true
                     } label: {
-                        Image(.instagramIcon)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 24, height: 24)
+                        Image(systemName: "plus.square")
                     }
+                    
+                    Image(systemName: "line.3.horizontal")
                 }
+            }
+        }
+        
+        // MARK: - Sheet
+        
+        .sheet(isPresented: $showSheet) {
+            VStack {
+                Text("수민언니 바보ㅋㅋ")
+                    .font(.pretendard(.medium, size: 20))
+                    .padding()
                 
-                ToolbarItem(placement: .topBarTrailing) {
-                    HStack(spacing: 20) {
-                        Button {
-                            moveToExample = true
-                        } label: {
-                            Image(systemName: "plus.square")
-                        }
-                        Image(systemName: "line.3.horizontal")
-                    }
+                Button("닫기") {
+                    showSheet = false
                 }
             }
-            
-            // MARK: - Sheet
-            
-            .sheet(isPresented: $showSheet) {
-                VStack {
-                    
-                    Text("수민언니 바보ㅋㅋ")
-                        .font(.pretendard(.medium, size: 20))
-                        .padding()
-                    
-                    Button("닫기") {
-                        showSheet = false
-                    }
-                }
-                .presentationDetents([.medium])
-            }
-            
-            // MARK: - Navigation
-            
-            .navigationDestination(isPresented: $moveToExample) {
-                ExampleView()
-            }
+            .presentationDetents([.medium])
+        }
+        
+        // MARK: - Navigation
+        
+        .navigationDestination(isPresented: $moveToExample) {
+            ExampleView()
         }
     }
 }
